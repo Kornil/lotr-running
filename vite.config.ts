ae2f338a45ef0b45da9868c53ef1a5f484360309
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2022-03-31 23:46:47 +0200

    Add base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({})],
+  base: "/lotr-running/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
