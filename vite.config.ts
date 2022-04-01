8e4ab963f66b65851b48bcab6cb1cab0aab29dbb
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2022-04-01 10:20:29 +0200

    Fix test config

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,21 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/assets/**",
+      "**.test.**",
+    ],
+    watch: false,
+    coverage: {
+      include: ["src/store", "src/components", "src/utils", "src/App.vue"],
+      reporter: ["text", "json"],
+    },
+  },
 });
